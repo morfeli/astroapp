8ec7b0c533af96c6acac159ed3042a3cbc02210a
morfeli <fmoreira95@hotmail.com>
2024-01-16 22:20:23 -0500

    feat: landing view

---
+++ astroApp.swift
@@ -0,0 +1,17 @@
+//
+//  astroApp.swift
+//  astro
+//
+//  Created by Felipe Moreira on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct astroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
